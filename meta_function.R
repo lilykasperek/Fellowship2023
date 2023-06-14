59290702c8d9626fa02e247b812d63fc8a25aa49
lilykasperek <lilykasperek92@gmail.com>
2023-06-14 13:12:24 -0400

    meta_function R script

---
+++ meta_function.R
@@ -0,0 +1,22 @@
+meta_function <- function(eval) {
+  tab <- pdf_text(eval)
+  first_page <- strsplit(tab[1], "\n")[[1]]
+  first_page_zeroes <- str_replace_all(first_page,
+                                       pattern = "-",
+                                       replacement = "0")
+  only_numbers <- str_extract_all(first_page_zeroes, "\\d+")
+  semester_string <- str_replace_all(first_page_zeroes[[2]], " ", "")
+  semester <- semester_string |> substring(5, 6)
+  
+  info <- noquote(first_page[[2]])
+  info_split <- strsplit(info, "-")
+  info_split[[1]][3] <- str_replace_all(info_split[[1]][3], " ", "")
+  info_split <- noquote(info_split)
+  meta_test <- tibble::tibble(course_num = only_numbers[[2]][3],
+                              subject = info_split[[1]][3],
+                              semester = semester, 
+                              year = only_numbers[[2]][1]) |>
+    mutate(course_num = as.double(course_num)) |>
+    mutate(year = as.double(year))
+  return(meta_test)
+}
