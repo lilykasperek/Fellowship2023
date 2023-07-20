62e3e5aee0682187639b4cfc5d4c9cb956717855
Matt Higham <mhigham@stlawu.edu>
2023-07-20 11:19:01 -0400

    toy data set for check all question.

---
+++ toy_check_all.R
@@ -0,0 +1,6 @@
+toy_df <- tibble::tibble(student_id = c("A", "B", "C", "D", "E", "F"),
+                         lecture = c(1, 1, 0, 1, 1, 0),
+                         group_work = c(1, 0, 0, 1, 1, 1),
+                         reading = c(0, 0, 0, 0, 0, 0),
+                         presentation = c(1, 1, 0, 1, 1, 1),
+                         code = c(1, 1, 1, 1, 1, 1))
